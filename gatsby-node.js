c6be4e382daf9af98ce92c4ad50431878b1b0e88
Akeem Allen <allenakeem8@protonmail.com>
2019-09-16 23:12:13 -0500

    Added .env.production file | Remember to secure it. Removed Rss feed due to breaking changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,11 +46,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `ContentfulBlogPost`) {
-    const value = createFilePath({ node, getNode })
+    // const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: node.slug,
     })
   }
 }
