34facf8204265c1cbf80d8169761233e5c09fc2b
Akeem Allen <allenakeem8@protonmail.com>
2019-10-09 10:41:46 -0500

    I feel stupid

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -2,4 +2,4 @@ if [ -z `ssh-keygen -F 74.207.224.133` ]; then
   ssh-keyscan -H 74.207.224.133 >> ~/.ssh/known_hosts
 fi
 
-echo "${password}"|ssh -tt -o akeem@74.207.224.133
+echo "${password}"|ssh -tt akeem@74.207.224.133
