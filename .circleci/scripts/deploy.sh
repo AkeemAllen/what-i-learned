dac1fd6f37eb148e5e16e7071f0aab40d7ff98af
Akeem Allen <allenakeem8@protonmail.com>
2019-10-09 18:16:08 -0500

    Attempting to download artifacts

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -2,8 +2,11 @@ if [ -z `ssh-keygen -F 74.207.224.133` ]; then
   ssh-keyscan -H 74.207.224.133 >> ~/.ssh/known_hosts
 fi
 
+curl https://circleci.com/api/v1.1/project/:vcs-type/:AkeemAllen/:what-i-learned/latest/artifacts?circle-token=:$CIRCLE_TOKEN
+
+echo "$(ls -a)"
+
 yes | sudo apt-get install expect
-echo "Got Here"
 
 # Successfuly logs me into server
 .circleci/scripts/cpyToServer.sh
