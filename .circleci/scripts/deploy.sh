7a9a14654f37e46b30cb72fcd421669a4d3bb32d
Akeem Allen <allenakeem8@protonmail.com>
2019-10-09 10:41:00 -0500

    Just made something cannon

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,1 +1,5 @@
-echo "${password}"|ssh -tt akeem@74.207.224.133
+if [ -z `ssh-keygen -F 74.207.224.133` ]; then
+  ssh-keyscan -H 74.207.224.133 >> ~/.ssh/known_hosts
+fi
+
+echo "${password}"|ssh -tt -o akeem@74.207.224.133
