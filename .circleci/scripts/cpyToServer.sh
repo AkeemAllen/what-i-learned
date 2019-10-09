fd289a83032ad3904048c80ec3202b0fda1868d6
Akeem Allen <allenakeem8@protonmail.com>
2019-10-09 18:57:51 -0500

    Sending file alone

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp ~/project/public/test.txt ssh akeem@74.207.224.133:~/
+spawn scp public/test.txt ssh akeem@74.207.224.133:~/
 
 expect "akeem@74.207.224.133's password"
 
