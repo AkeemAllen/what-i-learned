98061638640a2873b975285539cc756cfaed962b
Akeem Allen <allenakeem8@protonmail.com>
2019-10-09 11:11:59 -0500

    Removed -tt flag

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -2,6 +2,6 @@ if [ -z `ssh-keygen -F 74.207.224.133` ]; then
   ssh-keyscan -H 74.207.224.133 >> ~/.ssh/known_hosts
 fi
 
-yes | sudo apt-get -tt install expect
+yes | sudo apt-get install expect
 
 ssh akeem@74.207.224.133
