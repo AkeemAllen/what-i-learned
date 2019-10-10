a0e827ab12b87a644674745970add17f6f01415a
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 09:51:34 -0500

    Apparantly where I am is good enough

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -7,24 +7,3 @@ yes | sudo apt-get install sshpass
 
 # # Successfuly logs me into server
 .circleci/scripts/cpyToServer.sh
-
-# /usr/bin/expect <<EOD
-# spawn scp -r public/ ssh akeem@74.207.224.133:~/
-# expect "akeem@74.207.224.133's password"
-# send "akstar4321\r"
-# EOD
-
-# /usr/bin/expect <<EOD
-# spawn ssh akeem@74.207.224.133
-# expect "akeem@74.207.224.133's password"
-# send "akstar4321\r"
-
-# spawn echo "Hello"
-# interact
-# spawn ls -a
-# interact
-# EOD
-
-sshpass -p ${password} ssh akeem@74.207.224.133 "ls -a && mv public/ what_i_learned_blog && mv what_i_learned_blog/ /var/www/"
-# # Move public file to /var/www
-# # echo "$(sudo mv public/ /var/www/)"
