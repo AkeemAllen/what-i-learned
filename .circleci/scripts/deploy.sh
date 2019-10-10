f602974e212d4ae620104cb779d41fa40ab7b0e2
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 08:55:29 -0500

    Attempting to log in

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -16,6 +16,13 @@ expect "akeem@74.207.224.133's password"
 send "akstar4321\r"
 interact
 EOD
+
+/usr/bin/expect <<EOD
+spawn ssh akeem@74.207.224.133
+expect "akeem@74.207.224.133's password"
+send "akstar4321\r"
+interact
+EOD
 # # Move public file to /var/www
 # # echo "$(sudo mv public/ /var/www/)"
 echo "$(ls -a)"
