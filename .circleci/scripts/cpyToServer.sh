e80955e62bed475cbc5b139527288e00d3d57555
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 08:37:33 -0500

    Trying to move file to /var/www

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -7,9 +7,7 @@ spawn ssh akeem@74.207.224.133
 expect "akeem@74.207.224.133's password"
 send "akstar4321\r"
 
-interact
-
-spawn sudo mv public/ /var/www
+spawn sudo mv public/ what_i_learned_blog
 send "akstar4321\r"
 
 interact
