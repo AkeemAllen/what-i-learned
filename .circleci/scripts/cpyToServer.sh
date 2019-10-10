29bab3bc431176356a7fcd851e1f595f66856da9
Akeem Allen <allenakeem8@protonmail.com>
2019-10-09 19:12:35 -0500

    Trying to seal the deal

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/expect -f
 spawn scp -r public/ ssh akeem@74.207.224.133:~/
-
 expect "akeem@74.207.224.133's password"
+send "akstar4321\r"
 
+interact
+
+spawn ssh akeem@74.207.224.133
+expect "akeem@74.207.224.133's password"
+send "akstar4321\r"
+
+interact
+
+spawn sudo -i
+expect "[sudo] password for akeem"
 send "akstar4321\r"
 
 interact
