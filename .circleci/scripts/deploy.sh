6a9c45054faa3fc466546a1615110356dcc73b1d
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 22:38:15 -0500

    Forgot a then

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,7 +1,7 @@
 if [ $1 = "develop"]; then
     ip = 50.116.27.177
     server = admin@50.116.27.177
-  elif [$1 = "master"]
+  elif [$1 = "master"]; then
     ip = 74.207.224.133
     server = akeem@74.207.224.133
 fi
