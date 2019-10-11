04e52f702129cb2d8d105db3a8f6de8a7cf088f6
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 22:16:42 -0500

    Changed circleci configurations

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r public/ ssh akeem@74.207.224.133:~/
-expect "akeem@74.207.224.133's password"
-send "akstar4321\r"
+spawn scp -r public/ ssh $1:~/
+expect "$1's password"
+send "$2\r"
 interact
--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,8 +1,16 @@
-if [ -z `ssh-keygen -F 74.207.224.133` ]; then
-  ssh-keyscan -H 74.207.224.133 >> ~/.ssh/known_hosts
+if [ $1 = "develop"]; then
+    ip = 50.116.27.177
+    server = admin@50.116.27.177
+  elif [$1 = "master"]
+    ip = 74.207.224.133
+    server = akeem@74.207.224.133
+fi
+
+if [ -z `ssh-keygen -F $ip` ]; then
+  ssh-keyscan -H $ip >> ~/.ssh/known_hosts
 fi
 
 yes | sudo apt-get install expect
 
-# # Successfuly logs me into server
-.circleci/scripts/cpyToServer.sh
+# Successfuly logs me into server
+.circleci/scripts/cpyToServer.sh server ${password}
