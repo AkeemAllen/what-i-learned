c708b6ac784614132edb56c8c296ed53eff9b6bc
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 22:45:37 -0500

    Fixing Script

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -1,9 +1,9 @@
-if [ $1 = "develop"]; then
-    ip = 50.116.27.177
-    server = admin@50.116.27.177
-  elif [$1 = "master"]; then
-    ip = 74.207.224.133
-    server = akeem@74.207.224.133
+if [ $1 = "develop" ]; then
+    ip=50.116.27.177
+    server=admin@50.116.27.177
+  elif [ $1 = "master" ]; then
+    ip=74.207.224.133
+    server=akeem@74.207.224.133
 fi
 
 if [ -z `ssh-keygen -F $ip` ]; then
