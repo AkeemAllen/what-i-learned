3034bef18f02fb9761e1507a458ea0a48d97388d
Akeem Allen <allenakeem8@protonmail.com>
2019-10-10 23:09:47 -0500

    Changes

--- .circleci/scripts/cpyToDevelopServer.sh
+++ .circleci/scripts/cpyToDevelopServer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
 spawn scp -r public/ ssh admin@50.116.27.177:~/
 expect "admin@50.116.27.177's password"
-send "$1\r"
+send "akstar4321\r"
 interact
--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
 spawn scp -r public/ ssh akeem@74.207.224.133:~/
 expect "akeem@74.207.224.133's password"
-send "$1\r"
+send "akstar4321\r"
 interact
