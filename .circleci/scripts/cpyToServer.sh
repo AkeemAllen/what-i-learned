c2a9c4261ea6b98031cdf0dc24445e610959a476
Akeem Allen <allenakeem8@protonmail.com>
2019-10-27 13:56:07 -0500

    Concealing password

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/expect -f
+pass = ${1}
+echo ${pass}
+
 spawn scp -r public/ ssh akeem@74.207.224.133:~/
 expect "akeem@74.207.224.133's password"
-send `${1}\r`
+send "${pass}\r"
 interact
