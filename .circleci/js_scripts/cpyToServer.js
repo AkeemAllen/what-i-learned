31af5e36d8173655da1ce7d2d4e6b335dcfc0702
Akeem Allen <allenakeem8@protonmail.com>
2019-10-27 20:25:43 -0500

    Created new file

--- .circleci/js_scripts/cpyToServer.js
+++ .circleci/js_scripts/cpyToServer.js
@@ -1,3 +1,13 @@
 const exec = require("child_process").exec;
 
-exec(`spawn scp -r public/ ssh akeem@74.207.224.133:~/`);
+exec(
+  `spawn scp -r public/ ssh akeem@74.207.224.133:~/`,
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      console.log(stderr);
+    } else {
+      console.log(stdout);
+    }
+  }
+);
