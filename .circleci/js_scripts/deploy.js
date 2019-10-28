61cdd62747dc802514b3b582acd397bdf94245a2
Akeem Allen <allenakeem8@protonmail.com>
2019-10-27 19:28:44 -0500

    Rewriting scripts in javascript

---
+++ .circleci/js_scripts/deploy.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const exec = require("child_process").exec;
+
+const env = process.argv[2];
+let ip;
+let server;
+
+if (env == "develop") {
+  ip = `50.116.27.177`;
+  server = `admin@50.116.27.177`;
+} else if (env == "develop") {
+  ip = `74.207.224.133`;
+  server = `akeem@74.207.224.133`;
+}
+
+exec(
+  `echo ${process.env.password} | scp -r / ssh akeem@74.207.224.133:~/`,
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      console.log(stderr);
+    } else {
+      console.log(stdout);
+    }
+  }
+);
