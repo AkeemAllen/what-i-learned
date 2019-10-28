305d643682615cc32fe7c8925c50f34a5ae0bae7
Akeem Allen <allenakeem8@protonmail.com>
2019-10-27 20:43:08 -0500

    Setting variable differently

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/expect -f
-spawn scp -r public/ ssh akeem@74.207.224.133:~/
+set pass  [lindex $argv 0];
+
+spawn scp -r /project ssh akeem@74.207.224.133:~/
 expect "akeem@74.207.224.133's password"
 send "akstar4321\r"
 interact
