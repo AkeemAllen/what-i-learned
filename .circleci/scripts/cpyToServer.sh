81751ddc53788486e1cdbac2af47109c7ec97868
Akeem Allen <allenakeem8@protonmail.com>
2019-10-27 20:53:32 -0500

    Attempting to send entire project

--- .circleci/scripts/cpyToServer.sh
+++ .circleci/scripts/cpyToServer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 set pass  [lindex $argv 0];
 
-spawn scp -r .circleci/ ssh akeem@74.207.224.133:~/
+spawn scp -r ~/ ssh akeem@74.207.224.133:~/
 expect "akeem@74.207.224.133's password"
 send "${pass}\r"
 interact
