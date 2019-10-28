8de799b3e602039126af0d3cde16303ca309f504
Akeem Allen <allenakeem8@protonmail.com>
2019-10-27 20:35:56 -0500

    removed the -f flag

--- .circleci/js_scripts/cpyToServer.js
+++ .circleci/js_scripts/cpyToServer.js
@@ -1,7 +1,7 @@
 const exec = require("child_process").exec;
 
 exec(
-  `/usr/bin/expect -f spawn scp -r public/ ssh akeem@74.207.224.133:~/`,
+  `/usr/bin/expect spawn scp -r public/ ssh akeem@74.207.224.133:~/`,
   (err, stdout, stderr) => {
     if (err) {
       console.log(err);
