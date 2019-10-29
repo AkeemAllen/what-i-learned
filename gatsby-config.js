02eef5a80612dffbc6081ea64aee11613df93538
Akeem Allen <allenakeem8@protonmail.com>
2019-10-29 10:58:20 -0500

    Added Sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,6 +142,39 @@ module.exports = {
     //Provides drop-in support for SASS/SCSS stylesheets
     `gatsby-plugin-sass`,
     {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: ["/category/*", `/path/to/page`],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+  
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`,
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `daily`,
+              priority: 0.7,
+            };
+          }),
+      },
+    },
+    {
       //plugin for utilizing the Typography library with minimal configuration.
       resolve: `gatsby-plugin-typography`,
       options: {
