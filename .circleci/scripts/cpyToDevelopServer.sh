6370f9f6e254b368eddda34a6de31127c2c8cdb9
Akeem Roy Allen <allenakeem8@gmail.com>
2020-01-09 14:25:36 -0500

    Removed Circle Ci conneciton

--- .circleci/scripts/cpyToDevelopServer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/expect -f
-set pass  [lindex $argv 0];
-
-spawn scp -r ../project ssh akeem@74.207.224.133:~/
-expect "akeem@74.207.224.133's password"
-send "${pass}\r"
-interact
--- .circleci/scripts/cpyToServer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/expect -f
-set pass  [lindex $argv 0];
-
-spawn scp -r public/ ssh akeem@74.207.224.133:~/
-expect "akeem@74.207.224.133's password"
-send "${pass}\r"
-interact
